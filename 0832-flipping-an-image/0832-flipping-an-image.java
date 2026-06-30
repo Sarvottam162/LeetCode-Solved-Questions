28a435a59b317803b88c3852ded32ad21a36eaf4
Sarvottam kumar <147574477+Sarvottam162@users.noreply.github.com>
2026-06-30 19:30:33 +0530

    Time: 0 ms (100%), Space: 45.9 MB (17.58%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -1,20 +1,41 @@
+// class Solution {
+//     public int[][] flipAndInvertImage(int[][] image) {
+//         int n = image.length;
+
+//         for(int i = 0; i<n; i++){
+//             for(int j = 0; j<n/2; j++){
+//                 int temp = image[i][j];
+//                 image[i][j] = image[i][n - j - 1];
+//                 image[i][n - j - 1] = temp;
+//             }
+//         }
+
+//         for(int i = 0; i<n; i++){
+//             for(int j = 0; j<n; j++){
+//                image[i][j] = image[i][j] ^ 1;
+//             }
+//         }
+//         return image;
+//     }
+// }
+
+
+//by using an extra space res array both uper and lower solution is correct in upper we change everything in same array in lower we make new res arr 
+
+
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
         int n = image.length;
 
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n/2; j++){
-                int temp = image[i][j];
-                image[i][j] = image[i][n - j - 1];
-                image[i][n - j - 1] = temp;
-            }
-        }
+        int[][] res = new int[n][n];
 
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-               image[i][j] = image[i][j] ^ 1;
+        for(int i = 0 ; i<n; i++){
+            for(int j = 0 ; j<n; j++){
+                res[i][j] = image[i][n - j - 1] ^ 1;
             }
         }
-        return image;
+        return res;
+
     }
 }
+
