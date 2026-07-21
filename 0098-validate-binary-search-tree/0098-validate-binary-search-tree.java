63433e43e04c35dc7b43b77ef84444e5f7fb02e9
Sarvottam kumar <147574477+Sarvottam162@users.noreply.github.com>
2026-07-21 19:42:09 +0530

    Time: 0 ms (100%), Space: 44.8 MB (90.62%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,24 +13,45 @@
  *     }
  * }
  */
+//  method 1 
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         ArrayList<Integer> li = new ArrayList<>();
+
+//         helper(root , li);
+
+//         for(int i = 0; i< li.size() - 1 ; i++){
+//             if(li.get(i) >= li.get(i+1)) return false;
+//         }
+//         return true;
+//     }
+//     public void helper(TreeNode root , ArrayList<Integer> li){
+//         if(root == null) return;
+
+
+//         helper(root.left , li);
+//         li.add(root.val);
+//         helper(root.right , li);
+//         return;
+//     }
+// }
+
+// method 2
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        ArrayList<Integer> li = new ArrayList<>();
-
-        helper(root , li);
-
-        for(int i = 0; i< li.size() - 1 ; i++){
-            if(li.get(i) >= li.get(i+1)) return false;
-        }
-        return true;
+        return helper(root , Long.MIN_VALUE , Long.MAX_VALUE);
     }
-    public void helper(TreeNode root , ArrayList<Integer> li){
-        if(root == null) return;
+    public boolean helper(TreeNode root , long min , long max){
 
+        if(root == null) return true;
 
-        helper(root.left , li);
-        li.add(root.val);
-        helper(root.right , li);
-        return;
+        //check for false;
+
+        if(root.val >= max || root.val <= min) return false;
+
+        boolean left = helper(root.left , min , root.val);
+        boolean right = helper(root.right , root.val , max);
+        return left && right;
     }
 }
