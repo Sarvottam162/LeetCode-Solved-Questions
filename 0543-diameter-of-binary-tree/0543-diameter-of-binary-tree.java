ae5a4393da684afee48fc0f23d4c6dc15d22a6f8
Sarvottam kumar <147574477+Sarvottam162@users.noreply.github.com>
2026-07-24 09:55:34 +0530

    Time: 1 ms (46.25%), Space: 46.9 MB (73.71%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,30 +1,62 @@
+//method 1---
+
+// class Solution {
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         return diameter(root);
+//     }
+//     public int height(TreeNode root){
+
+//         if(root == null) return 0;
+
+//         int leftHeight = height(root.left);
+//         int rightHeight = height(root.right);
+
+//         return Math.max(leftHeight , rightHeight) + 1;
+
+//     }
+//     public int diameter(TreeNode root){
+
+//         if(root == null) return 0;
+
+//         int leftDia = diameter(root.left);
+//         int leftHeight = height(root.left);
+
+//         int rightDia = diameter(root.right);
+//         int rightHeight = height(root.right);
+
+//         int selfDia = leftHeight + rightHeight;
+
+//         return Math.max(Math.max(leftDia , rightDia) , selfDia);
+//     }
+// }
+
+//method 2 ----
+
 
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        return diameter(root);
+        return helper(root).diameter;
     }
-    public int height(TreeNode root){
 
-        if(root == null) return 0;
+    public info helper(TreeNode root){
 
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
+        if(root == null) return new info(0,0);
 
-        return Math.max(leftHeight , rightHeight) + 1;
+        info leftinfo = helper(root.left);
+        info rightinfo = helper(root.right);
 
+        int diameter = Math.max(Math.max(leftinfo.diameter , rightinfo.diameter),leftinfo.height + rightinfo.height);
+        int height = Math.max(leftinfo.height , rightinfo.height)+ 1;
+
+        return new info(diameter , height);
     }
-    public int diameter(TreeNode root){
+}
 
-        if(root == null) return 0;
-
-        int leftDia = diameter(root.left);
-        int leftHeight = height(root.left);
-
-        int rightDia = diameter(root.right);
-        int rightHeight = height(root.right);
-
-        int selfDia = leftHeight + rightHeight;
-
-        return Math.max(Math.max(leftDia , rightDia) , selfDia);
+class info {
+    int diameter;
+    int height;
+    info(int diameter , int height){
+        this.diameter = diameter;
+        this.height = height;
     }
 }
