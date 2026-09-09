3604d4c07d1401556247418d8acb6e071243f8ef
Sarvottam kumar <147574477+Sarvottam162@users.noreply.github.com>
2026-09-09 14:04:21 +0530

    Time: 44 ms (50.58%), Space: 48 MB (28.09%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,26 +1,48 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashSet<Character> set = new HashSet<>();
-        int windowStart = 0 ;
-        int windowEnd = 0 ;
+
+        // method 1
+
+        // HashSet<Character> set = new HashSet<>();
+        // int windowStart = 0 ;
+        // int windowEnd = 0 ;
+        // int n = s.length();
+        // int length = 0;
+
+        // while(windowEnd < n){
+        //     char ch = s.charAt(windowEnd);
+        //     if(!set.contains(ch)){
+        //         set.add(ch);
+        //         int currLength = set.size();
+        //         length = Math.max(length , currLength);
+        //     }
+        //     else{
+        //         while(windowStart < windowEnd && set.contains(ch)){
+        //             char ch1 = s.charAt(windowStart);
+        //             set.remove(ch1);
+        //             windowStart++;
+        //         }
+        //         set.add(ch);
+        //     }
+        //     windowEnd++;
+        // }
+        // return length;
+
+        // method 2
+
+        HashMap<Character , Integer> map = new HashMap<>();
+        int windowStart = 0;
+        int windowEnd = 0;
         int n = s.length();
         int length = 0;
 
         while(windowEnd < n){
             char ch = s.charAt(windowEnd);
-            if(!set.contains(ch)){
-                set.add(ch);
-                int currLength = set.size();
-                length = Math.max(length , currLength);
+            if(map.containsKey(ch) && map.get(ch) >= windowStart){
+                windowStart = map.get(ch) + 1;
             }
-            else{
-                while(windowStart < windowEnd && set.contains(ch)){
-                    char ch1 = s.charAt(windowStart);
-                    set.remove(ch1);
-                    windowStart++;
-                }
-                set.add(ch);
-            }
+            map.put(ch , windowEnd);
+            length = Math.max(length,windowEnd - windowStart + 1);
             windowEnd++;
         }
         return length;
